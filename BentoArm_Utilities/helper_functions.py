73438cc9cab12753ec6d41ea50ee3b2196329381
Justin <jkf@ualberta.ca>
2023-08-24 13:55:37 -0700

    moved checksum function

--- BentoArm_Utilities/helper_functions.py
+++ BentoArm_Utilities/helper_functions.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def change_scale(old_min, old_max, new_min, new_max, value):
     """
     Converts value in [old_min, old_max] proportionally to [new_min, new_max]
@@ -10,3 +12,11 @@ def clamp_range(min_val, max_val, value):
     Clamps value within [min_val,max_val]
     """
     return max(min_val, min(value, max_val))
+
+def checksum_fcn(packet):
+    """The checksum is calculated via summing up all the bytes and taking the bitwise complement (~/not operator) of
+    the sum cast to an uint8 / char / 8 bits, the header (first two bytes being 0xFF) and checksum (last bytes) are not
+    used for the checksum"""
+    summed_packet = sum(packet)
+    checksum = ~np.uint8(summed_packet)
+    return checksum
