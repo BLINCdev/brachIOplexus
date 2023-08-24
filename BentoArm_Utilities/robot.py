ceb3319e712731e43668c123f5240d6f9b3e6619
Justin <jkf@ualberta.ca>
2023-08-24 13:55:48 -0700

    better docstrings

--- BentoArm_Utilities/robot.py
+++ BentoArm_Utilities/robot.py
@@ -1,5 +1,4 @@
-from socket_handler import checksum_fcn
-from helper_functions import clamp_range, change_scale
+from helper_functions import clamp_range, change_scale, checksum_fcn
 from math import pi
 
 MIN_ANGLES = [1028, 1784, 1028, 790, 1928]
@@ -12,125 +11,60 @@ DYNA_MIN = 0
 DYNA_MAX = 4095
 
 
-class ServoInfo(object):
-    """
-    Abstract class to hold basic implementation for any servo related information
-
-    Attributes:
-        id (int): ID Number representing motor in bento arm, ID's are in range [1, # Of Joints]
-        position (int): Position of joint in dynamixel range [0, 4095]
-        velocity (int): Velocity for joint in dynamix range 1024 +- x
-        load (int): Load / Torque of joint
-        temp (int): Temperature of joint
-    """
-    def __init__(self):
-        self.id = None
-        self.position = None  # The current position value of the servo
-        self.velocity = None  # The current moving speed of the servo
-        self.load = None  # The current load applied to the servo
-        self.temp = None  # The internal temperature of the servo in deg
-
-
-class MxSeries(ServoInfo):
-    """
-    Specific instance of all servos of the mx series type (i.e mx-28, mx-64, mx-106)
-    """
-
-    def __init__(self, index):
-        """
-        Args:
-            index: Index of motor in [0, # of Joints - 1]
-
-        Attributes:
-            pmin (int): Min position for joint in dynamixel range [0,4095]
-            pmax (int): Max position for joint in dynamixel range [0,4095]
-            radians_min (float): Min position for joint in radians by default in [-π, π]
-            radians_max (float): Max position for joint in radians by default in [-π, π]
-            vmin (int): Min velocity for joint in dynamix range 1024 - x
-            vmax (int): Min velocity for joint in dynamix range 1024 + x
-            loadmin (int): Min effort / load / torque
-            loadmax (int): Max effort / load / torque
-            maxtemp (int): Default (80)
-
-        """
-        super().__init__()
-        self.id = index + 1
-        self.pmin = MIN_ANGLES[index] - BUFFER
-        self.pmax = MAX_ANGLES[index] + BUFFER
-        self.radians_min = self.dyna_to_radians(self.pmin, zero_to_2pi=False)
-        self.radians_max = self.dyna_to_radians(self.pmax, zero_to_2pi=False)
-        self.vmin = 1024 - V_RANGE[index] - BUFFER
-        self.vmax = 1024 + V_RANGE[index] + BUFFER
-        self.loadmin = 1024 - LOAD_RANGE[index] - BUFFER
-        self.loadmax = 1024 + LOAD_RANGE[index] + BUFFER
-        self.maxtemp = MAX_TEMP
-        self.state = None
-
-    def normalized_to_dyna_pos_range(self, value):
-        """Takes in a value in [0,1] and converts it to a dyna position value within that particular motors range"""
-        return change_scale(old_min=0, old_max=1, new_min=self.pmin, new_max=self.pmax, old_value=value)
-
-    def normalized_to_dyna_vel_range(self, value):
-        """Takes in a value in [0,1] and converts it to a dyna velocity value within that particular motors range"""
-        return int(change_scale(old_min=0, old_max=1, new_min=self.vmin, new_max=self.vmax, value=value))
-
-    def get_normalized_joint_position(self):
-        """Converts current dyna position values self.position to a value in [0,1]"""
-        normalized_position = change_scale(old_min=self.pmin, old_max=self.pmax, new_min=0, new_max=1,
-                                           value=self.position)
-        return normalized_position
-
-    def get_clamped_dyna_joint_position(self, position):
-        """Clamps given value to within allowable range for the particular joint"""
-        return clamp_range(min_val=self.pmin, max_val=self.pmax, value=position)
-
-    def dyna_to_radians(self, value, zero_to_2pi=False):
-        """
-        Converts a dyna value in range [0,4095] to radians range [-π,π] or [0,2π]
-        Args:
-            value (int): Dynamixel position value
-            zero_to_2pi (bool): If true will convert to range [0,2π] else [-π,π]
-
-        Returns:
-            Dynamixel position in radians range of choice
-        """
-        if zero_to_2pi:
-            return change_scale(DYNA_MIN, DYNA_MAX, 0, 2 * pi, value)
-        else:
-            return change_scale(DYNA_MIN, DYNA_MAX, -pi, pi, value)
-
-    def radians_to_dyna(self, value, normalized=True, zero_to_2pi=False):
-        """
-        Converts a radians value in [-π,π] or [0,2π] to a dyna range in [0,4095]
-        Args:
-            value (float):
-            normalized (bool): If true returns value in [0,1] else [0,4095]
-            zero_to_2pi:
-
-        Returns:
-
-        """
-        if zero_to_2pi:
-            old_min = 0
-            old_max = 2 * pi
-        else:
-            old_min = -pi
-            old_max = pi
-        if normalized:
-            return change_scale(old_min, old_max, 0, 1, value)
-        else:
-            return change_scale(old_min, old_max, DYNA_MIN, DYNA_MAX, value)
-
-
 class Robot:
-    def __init__(self):
+    def __init__(self, normalized=True):
         """
-        Class for BentoArm which is just a collection of joints
+        Class for BentoArm which is just a collection of MxSeries joints.  There are two playback ways we can represent a
+        joint state.
+
+        - normalized state: position is in range [0,1], useful as it derives nicely for deep learning applications
+        - dynamixel state: standard representation used by dynamixel [0,4095]
+
+        Since there are many ways to represent states the code avoids doing as many state conversions as possible
+        putting the onus on the user to change ranges as needed.  There are some functions that work in the range of
+        [-π,π] as this is the range used by the inverse kinematics library IKPy.
+
+        Args:
+            normalized:  If true joint state is represented via a [0, 1] range else the dynamixel range [0,4095]
 
         Attributes:
             joints (list): A list of MxSeries joints
-            hand_states (dict): Hand states in the range of [-pi, pi] mainly just used by IKPY
+            hand_states (dict): Hand states in the range of [-π,π] mainly just used by IKPY
+
+        Examples:
+        ```python
+        from robot import Robot
+        from helper_functions import change_scale
+        from math import radians, pi
+        from socket_handler import SocketHandler
+
+
+        sock = SocketHandler
+
+        robot.update_joints_from_packet(sock.read_packet())  # Typically done in a thread
+
+        # Get State, Build & Send Action (Rotate Shoulder by 45 Deg.) [NORMALIZED]
+        robot = Robot(normalized=True)
+        state = robot.get_joints(normalized = True)
+        state[0] += change_scale(old_min = 0,
+                                 old_max = 2*pi,
+                                 new_min = 0,
+                                 new_max = 1,
+                                 value = radians(45)
+        sock.send_packet(robot.build_joints_packet(joint_positions=state, normalized=True)
+
+        # Get State, Build & Send Action (Rotate Shoulder by 45 Deg.) [DYNA]
+        robot = Robot(normalized=False)
+        state = robot.get_joints(normalized = FALSE)
+        state[0] += change_scale(old_min = 0,
+                                 old_max = 360,
+                                 new_min = DYNA_MIN,
+                                 new_max = DYNA_MAX,
+                                 value = 45
+        sock.send_packet(robot.build_joints_packet(joint_positions=state, normalized=False)
+        ```
         """
+        self.normalized = normalized
         self.joints = [MxSeries(index=i) for i in range(5)]
         self.hand_states = {"closed": -0.15, "mid": 0.24, "open": 1.1428}
 
@@ -156,7 +90,7 @@ class Robot:
             self.joints[idx].temp = packet[i + 7]
             self.joints[idx].state = packet[i + 8]
 
-    def build_joints_packet(self, joint_positions, velocities=(1024,) * 5, normalized=True):
+    def build_joints_packet(self, joint_positions, velocities=(1024,) * 5):
         """
         Builds a bytearray packet of velocities and positions for each motor to be sent/parsed by the BracIOplexus
         software to create motor commands. This packet structure was developed by us, not brachIOplexus.  It is a simple
@@ -166,36 +100,37 @@ class Robot:
 
         Args:
             joint_positions (list): A collection of 5 values of either [0,4096] or [0,1] representing position of each motor
-            velocities (tuple): Velocities, just use default of 1024 for now, TODO add velocity control
-            normalized (bool): If normalized joint_positions values should be [0,1] else [0,4096] (raw dynamixel values)
+            velocities (tuple): Velocities, just use default of 1024 for now
 
         Returns:
             bytearray: packet to be sent to BrachIOplexus via udp
+
+        Todo:
+            This yet supports velocity control, just using default values for now
         """
 
-        # A bunch of safety checks since sending raw values to bento arm
+        # Safety checks since sending raw values to bento arm can be dangerous
         assert (len(joint_positions) == len(self.joints)), "Invalid positions length, pass for all 5 joints"
         assert (len(velocities) == len(self.joints)), "Invalid velocities length, pass for all 5 joints"
 
-        if normalized:  # [0,1]
+        if self.normalized:  # [0,1]
             """If normalized, convert to individual motors dyna range"""
             joint_positions = [float(i) for i in joint_positions]
             for i in range(len(self.joints)):
-                # Converting normalized to dyna range always ensures it's within allowable range
-                # If there are errors in the normalized range, this cannot be fixed here.
-                joint_positions[i] = self.joints[i].normalized_to_dyna_pos_range((joint_positions[i]))
+                joint_positions[i] = self.joints[i].normalized_to_dyna_pos_range((joint_positions[
+                    i]))  # Converting normalized to dyna range always ensures it's within allowable range
             joint_positions = [int(i) for i in joint_positions]
         else:  # [0,4095]
             """If raw values, assert if in range"""
             joint_positions = [int(i) for i in joint_positions]
             for i in range(len(self.joints)):
                 # Check positions
-                assert (self.joints[i].pmin <= joint_positions[i] <= self.joints[
-                    i].pmax), "Make sure servo positions are within valid range"
+                assert (self.joints[i].position_min <= joint_positions[i] <= self.joints[
+                    i].postion_max), "Make sure servo positions are within valid range"
 
                 # Check velocities
-                assert (velocities[i] in range(self.joints[i].vmin, self.joints[
-                    i].vmax)), "Make sure servo velocities are within valid range"
+                assert (velocities[i] in range(self.joints[i].velocity_min, self.joints[
+                    i].velocity_max)), "Make sure servo velocities are within valid range"
 
         packet = [0xFF, 0xFF, 4 * len(self.joints)]  # Length = 4 bytes (pos + vel) per joint
         for i in range(len(self.joints)):
@@ -207,12 +142,9 @@ class Robot:
         packet.append(checksum_fcn(packet[2:]))  # Append checksum function to end
         return bytearray(packet)
 
-    def print_joints(self, normalized=False):
+    def print_joints(self):
         """
         Prints the array of Robots current joint positions
-
-        Args:
-            normalized (bool): If true returns joint values within [0,1] relative to their range, else [pmin, pmax]
 
         Returns:
             None
@@ -220,22 +152,132 @@ class Robot:
         """
 
         for joint in self.joints:
-            if normalized:
+            if self.normalized:
                 print(f"ID {joint.id}: {joint.get_normalized_joint_position():.2f}  ", end="")
             else:
                 print(f"ID {joint.id}: {joint.position}  ", end="")
         print("")
 
-    def get_joints(self, normalized=False):
+    def get_joints(self):
         """
-        Args:
-            normalized (bool): If true returns joint values within [0,1] relative to their range, else [pmin, pmax]
-
         Returns:
             list: current joint positions for Robot
         """
 
-        if normalized:
+        if self.normalized:
             return [joint.get_normalized_joint_position() for joint in self.joints]
         else:
             return [joint.position for joint in self.joints]
+
+
+class ServoInfo(object):
+    """
+    Abstract class to hold basic implementation for any servo related information
+
+    Attributes:
+        id (int): ID Number representing motor in bento arm, ID's are in range [1, # Of Joints]
+        position (int): Position of joint in dynamixel range [0, 4095]
+        velocity (int): Velocity for joint in dynamix range 1024 +- x
+        load (int): Load / Torque of joint
+        temp (int): Temperature of joint
+    """
+
+    def __init__(self):
+        self.id = None
+        self.position = None  # The current position value of the servo
+        self.velocity = None  # The current moving speed of the servo
+        self.load = None  # The current load applied to the servo
+        self.temp = None  # The internal temperature of the servo in deg
+
+
+class MxSeries(ServoInfo):
+    """
+    Specific instance of all servos of the mx series type (i.e mx-28, mx-64, mx-106)
+    """
+
+    def __init__(self, index):
+        """
+        Args:
+            index: Index of motor in [0, # of Joints - 1]
+
+        Attributes:
+            position_min (int): Min position for joint in dynamixel range [0,4095]
+            position_max (int): Max position for joint in dynamixel range [0,4095]
+            radians_min (float): Min position for joint in radians by default in [-π, π]
+            radians_max (float): Max position for joint in radians by default in [-π, π]
+            velocity_min (int): Min velocity for joint in dynamix range 1024 - x
+            velocity_max (int): Min velocity for joint in dynamix range 1024 + x
+            load_min (int): Min effort / load / torque
+            load_max (int): Max effort / load / torque
+            max_temp (int): Default (80)
+
+        """
+        super().__init__()
+        self.id = index + 1
+        self.position_min = MIN_ANGLES[index] - BUFFER
+        self.postion_max = MAX_ANGLES[index] + BUFFER
+        self.radians_min = self.dyna_to_radians(self.position_min, zero_to_2pi=False)
+        self.radians_max = self.dyna_to_radians(self.postion_max, zero_to_2pi=False)
+        self.velocity_min = 1024 - V_RANGE[index] - BUFFER
+        self.velocity_max = 1024 + V_RANGE[index] + BUFFER
+        self.load_min = 1024 - LOAD_RANGE[index] - BUFFER
+        self.load_max = 1024 + LOAD_RANGE[index] + BUFFER
+        self.max_temp = MAX_TEMP
+        self.state = None
+
+    def normalized_to_dyna_pos_range(self, value):
+        """Takes in a value in [0,1] and converts it to a dyna position value within that particular motors range"""
+        return change_scale(old_min=0, old_max=1, new_min=self.position_min, new_max=self.postion_max, value=value)
+
+    def normalized_to_dyna_vel_range(self, value):
+        """Takes in a value in [0,1] and converts it to a dyna velocity value within that particular motors range"""
+        return int(
+            change_scale(old_min=0, old_max=1, new_min=self.velocity_min, new_max=self.velocity_max, value=value))
+
+    def get_normalized_joint_position(self):
+        """Converts current dyna position values self.position to a value in [0,1]"""
+        normalized_position = change_scale(old_min=self.position_min, old_max=self.postion_max, new_min=0, new_max=1,
+                                           value=self.position)
+        return normalized_position
+
+    def get_clamped_dyna_joint_position(self, position):
+        """Clamps given value to within allowable range for the particular joint"""
+        return clamp_range(min_val=self.position_min, max_val=self.postion_max, value=position)
+
+    def dyna_to_radians(self, value, zero_to_2pi=False):
+        """
+        Converts a dyna value in range [0,4095] to radians range [-π,π] or [0,2π]
+        Args:
+            value (int): Dynamixel position value
+            zero_to_2pi (bool): If true will convert to range [0,2π] else [-π,π]
+
+        Returns:
+            Dynamixel position in radians range of choice
+        """
+        if zero_to_2pi:
+            return change_scale(DYNA_MIN, DYNA_MAX, 0, 2 * pi, value)
+        else:
+            return change_scale(DYNA_MIN, DYNA_MAX, -pi, pi, value)
+
+    def radians_to_dyna(self, value, normalized=True, zero_to_2pi=False):
+        """
+        Converts a radians value in [-π,π] or [0,2π] to a dyna range in [0,4095]
+
+        Args:
+            value (float): radians value to be converted either in [0,2π] else [-π,π]
+            normalized (bool): If true returns value in [0,1] else [0,4095]
+            zero_to_2pi (bool): If true will convert from range [0,2π] else [-π,π]
+
+        Returns:
+
+        """
+        if zero_to_2pi:
+            old_min = 0
+            old_max = 2 * pi
+        else:
+            old_min = -pi
+            old_max = pi
+        if normalized:
+            return change_scale(old_min, old_max, 0, 1, value)
+        else:
+            return change_scale(old_min, old_max, DYNA_MIN, DYNA_MAX, value)
--- BentoArm_Utilities/socket_handler.py
+++ BentoArm_Utilities/socket_handler.py
@@ -1,35 +1,68 @@
-import numpy as np
+from helper_functions import checksum_fcn
 import socket
 import select
 
-
-def checksum_fcn(packet):
-    """The checksum is calculated via summing up all the bytes and taking the bitwise complement (~/not operator) of
-    the sum cast to an uint8 / char / 8 bits, the header (first two bytes being 0xFF) and checksum (last bytes) are not
-    used for the checksum"""
-    summed_packet = sum(packet)
-    checksum = ~np.uint8(summed_packet)
-    return checksum
-
-
 class SocketHandler:
     def __init__(self):
-        self.portTX = 30006  # The port that this script will send data to
-        self.portRX = 30007  # The port that this script will receive data from
-        self.udpIP = "127.0.0.1"  # The IP address of the computer/program that you want to send data to. Use 127.0.0.1 when communicating between two programs on the same computer.
-        self.bufferSize = 1024  # Buffer size for incoming bytes, this is pretty standard
+        """
+        Socket handler used for UDP communication with BrachIOPlexus uses one port for sending and another for
+        receiving.  Due to the dual port nature of this setup it is best to have one thread constantly looking for
+        packets and another sending when needed.
+
+        Example:
+            ```python
+            from threading import thread
+            from socket_handler import SocketHandler()
+
+            socket = SocketHandler()
+
+            # Receive Loop
+            def get_state(self):
+                while True:
+                packet = self.socket_handler.read_packet()
+                if not packet:
+                    continue
+
+                data = do_something("With Packet")
+
+                time.sleep(0.001)
+
+            thread = Thread(target=get_state)
+            thread.start()
+
+            # Send loop
+            while True:
+                packet = "BUILD PACKET HERE"
+                socket.send_packet(packet)
+            ```
+
+
+        Attributes:
+            port_tx (int): The port that this script will send data to.  Default (30006)
+            port_rx (int): The port that this script will receive data from. Default (30007)
+            udp_ip (string): The IP address of the computer/program that you want to send data to. Use 127.0.0.1 when communicating between two programs on the same computer.
+            bufferSize (int): Buffer size for incoming bytes.  Default (1024)
+            sock (socket): UDP socket
+        """
+        self.port_tx = 30006
+        self.port_rx = 30007
+        self.udp_ip = "127.0.0.1"
+        self.buffer_size = 1024
         self.failed_packets = 0
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind((self.udpIP, self.portRX))
+        self.sock.bind((self.udp_ip, self.port_rx))
         self.sock.setblocking(False)  # We don't want RX to block anything
 
     def read_packet(self):
-        """Receives a packet, checks the checksum, and returns the message"""
+        """
+        Receives a BentoArm state packet (more about packet structure in README.md) , checks the checksum, and returns
+        the state message which contains all the information saved in a robot.py ServoInfo class
+        """
         result = select.select([self.sock], [], [], 0.0)
         if len(result[0]) == 0:
             return None
-        message = result[0][0].recv(self.bufferSize)
+        message = result[0][0].recv(self.buffer_size)
         checksum = checksum_fcn(message[2:-1])  # Remove header and checksum
         if message[0] == 0xFF and message[1] == 0xFF and checksum == message[-1]:
             # If header is correct and checksum is passed return the message
@@ -39,5 +72,5 @@ class SocketHandler:
             return None
 
     def send_packet(self, packet):
-        """Sends the packet, typically in a Bento arm packet message"""
-        self.sock.sendto(packet, (self.udpIP, self.portTX))
+        """Sends the packet, typically a Bento arm packet message"""
+        self.sock.sendto(packet, (self.udp_ip, self.port_tx))
