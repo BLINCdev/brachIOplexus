7f41f810ca273f5a53cc0ef4d7a9f17374e1755a
Justin <jkf@ualberta.ca>
2023-08-25 11:40:42 -0700

    moved fill state

--- BentoArm_Utilities/helper_functions.py
+++ BentoArm_Utilities/helper_functions.py
@@ -13,6 +13,7 @@ def clamp_range(min_val, max_val, value):
     """
     return max(min_val, min(value, max_val))
 
+
 def checksum_fcn(packet):
     """The checksum is calculated via summing up all the bytes and taking the bitwise complement (~/not operator) of
     the sum cast to an uint8 / char / 8 bits, the header (first two bytes being 0xFF) and checksum (last bytes) are not
@@ -20,3 +21,16 @@ def checksum_fcn(packet):
     summed_packet = sum(packet)
     checksum = ~np.uint8(summed_packet)
     return checksum
+
+
+def fill_state(state):
+    """If a state of 4 values is passed, it will fill in all the fixed state values of 0"""
+    # Default full state value
+    if len(state) == 9:
+        return state
+    # Else append 4 joint state to nine joint state to account for fixed joints
+    for i in range(3):
+        state.insert(0, None)
+    for i in range(2):
+        state.append(None)
+    return state
