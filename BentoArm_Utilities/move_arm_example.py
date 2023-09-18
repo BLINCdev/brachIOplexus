a626cce5de9b7d9d470efef1c3120e7895201eec
Justin <jkf@ualberta.ca>
2023-09-18 12:43:34 -0700

    more verbose move arm example

--- BentoArm_Utilities/move_arm_example.py
+++ BentoArm_Utilities/move_arm_example.py
@@ -10,20 +10,23 @@ ik = InverseKinematics(robot_obj=robot)
 positions = [0.5, 0.5, 0.5, 0.5, 0.5]
 socket.send_packet(robot.build_joints_packet(joint_positions=positions))
 
-time.sleep(3)
+time.sleep(5)
 
 goal = [52.7, 0, 15.55]
 positions = ik.get_joints_for_goal_xyz(goal, hand_state='open')
+print(f'Goal: {goal}  Hand: Open  Positions: {positions}')
 socket.send_packet(robot.build_joints_packet(joint_positions=positions))
 
-time.sleep(3)
+time.sleep(5)
 
 goal = [52.7, 0, 19.55]
 positions = ik.get_joints_for_goal_xyz(goal, hand_state='closed')
+print(f'Goal: {goal}  Hand: Closed  Positions: {positions}')
 socket.send_packet(robot.build_joints_packet(joint_positions=positions))
 
-time.sleep(3)
+time.sleep(5)
 
 goal = [51, 0, 25]
 positions = ik.get_joints_for_goal_xyz(goal, hand_state='mid')
+print(f'Goal: {goal}  Hand: Mid  Positions: {positions}')
 socket.send_packet(robot.build_joints_packet(joint_positions=positions))
